7637fdec7160ee2e9824b8917700fc48923cd53a
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-12-25 04:29:48 +0530

    Animation Text Ready

--- development/src/App.svelte
+++ development/src/App.svelte
@@ -1,8 +1,10 @@
 <script>
 	import Hero from "./parts/hero.svelte";
+	import Loader from "./parts/loader.svelte";
 	import "./styles/main.css";
 </script>
 
 <body>
+	<Loader/>
 	<Hero/>
 </body>
---
+++ development/src/parts/loader.svelte
@@ -0,0 +1,44 @@
+<section id="loader">
+<pre>
+
+<div class="same_space">
+
+__________________________________________________________________________________________
+|                                                                                                                                                                    |
+|     ***********             ***                ***          ***          **               **      ************         ***            **********     <span style="margin-left: 1.5px;">|</span>
+|     **                       **  **             **  **      **  **         **               **      **          **        **  **              **           <span style="margin-left: 1px;">|</span>
+|     **                      **     **           **    **  **    **         **               **      **          **       **    **             **           <span style="margin-left: 1px;">|</span>
+|     ***********         **********          **     ****     **         **               **      ************     *********            **           <span style="margin-left: 2px;">|</span>
+|                 **        ***********         **                **          **            **       **        **       **********           **           <span style="margin-left: 4px;">|</span>
+|                 **       **           **        **                **            **        **         **         **     **          **          **           <span style="margin-left: 2px;">|</span>
+|     ***********       **            **        **                **             ********          **           **   **           **     *********      <span style="margin-left: 4px;">|</span>       
+|_________________________________________________________________________________________|
+
+</div>
+
+Booting System...
+
+Boot Sucessfull!!
+
+Loading Assests
+
+Image 1 - Hero
+Image 2 - Hero_Mobile
+
+Font 1 - Apex MK2
+Font 2 - Cyberpunk
+Font 3 - Vox Round
+Font 4 - Munro
+
+Stylesheet 1 - Main
+
+Svelte 1 - Main
+Svelte 2 - App
+Svelte 3 - Hero
+
+Assets Loaded Successfully
+
+Initializing Now...
+
+</pre>
+</section>
