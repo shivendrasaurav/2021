8cf3ba873587989512ab6ed00b1d27f1a00c324b
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-12-25 12:13:24 +0530

    Loading Canvas

--- development/src/parts/loader.svelte
+++ development/src/parts/loader.svelte
@@ -1,8 +1,23 @@
 <section id="loader">
     <!-- svelte-ignore a11y-media-has-caption -->
-	<video
+	<video class="loading_video"
 		poster="https://sveltejs.github.io/assets/caminandes-llamigos.jpg"
-		src="https://sveltejs.github.io/assets/caminandes-llamigos.mp4" format="mp4" autoplay>
+		src="https://raw.githubusercontent.com/shivendrasaurav/2021/main/development/src/parts/PLA.mp4" format="mp4" autoplay>
 			<track kind="captions"/>
     </video>
 </section>
+
+
+<style>
+    .loading_video{
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 100vh;
+    }
+    #loader{
+        height: 100vh;
+        width: 100vh;
+        background: red;
+    }
+</style>
