67847542544aa22f31ff68eaff2fa6c168b44b5c
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-12-26 00:02:14 +0530

    Added Socials

--- development/src/parts/hero.svelte
+++ development/src/parts/hero.svelte
@@ -1,6 +1,6 @@
 <script>
     function change_cmd_text_1(){
-        document.getElementById("command").innerHTML = "SAMURAI &gt; Portfolio of Shivendra Saurav";
+        document.getElementById("command").innerHTML = "SAMURAI &gt; portfolio of Shivendra Saurav";
     }
 
     function change_cmd_text_2(){
@@ -8,12 +8,17 @@
     }
 
     function change_cmd_text_3(){
-        document.getElementById("command").innerHTML = "run TheCodeDrop";
+        document.getElementById("command").innerHTML = "SAMURAI &gt; run yt TheCodeDrop";
+    }
+
+    function change_cmd_text_4(){
+        document.getElementById("command").innerHTML = "SAMURAI &gt; fetch skill_tree";
     }
 
     function default_text(){
         document.getElementById("command").innerHTML = "SAMURAI &gt; ";
     }
+
 </script>
 
 <section id="hero">
@@ -24,9 +29,9 @@
         </div>
     </div>
 
-    <div class="res_topic_wrapper" on:mouseover={change_cmd_text_2} on:mouseleave={default_text}>
+    <div class="res_topic_wrapper" on:mouseover={change_cmd_text_4} on:mouseleave={default_text}>
         <div class="res_topic">
-            Resume
+            Skills
         </div>
     </div>
 
@@ -42,4 +47,14 @@
         </div>
     </div>
 
+    <div class="social_wrapper">
+
+        <a href="https://www.youtube.com/channel/UCnEIkvnbpbQyetPk_K9QNQg?confirm_subscribe=1"><img src="./static/social/youtube.png" alt="yt" /></a>
+        <a href="https://www.linkedin.com/in/shivendra-saurav-80a8ba191/"><img src="./static/social/linkedin.png" alt="li" /></a>
+        <a href="https://github.com/shivendrasaurav"><img src="./static/social/github.png" alt="gh" /></a>
+        <a href="https://twitter.com/ShivendraSaurav"><img src="./static/social/twitter.png" alt="tw" /></a>
+        <a href="https://www.instagram.com/shivendrasaurav/"><img src="./static/social/instagram.png" alt="ig" /></a>
+
+
+    </div>
 </section>
