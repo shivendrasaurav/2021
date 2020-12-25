9a90c6ff0dad72774aeddc7dcb3e8e5a5f361d97
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-12-26 01:05:43 +0530

    Added Responsiveness

--- development/src/parts/loader.svelte
+++ development/src/parts/loader.svelte
@@ -2,7 +2,7 @@
     function hide_loading (){
         setInterval(function(){
             document.getElementById("loader").style.display="none";
-        }, 170);
+        }, 17010);
     }
 </script>
 
