b3043f796fc5b3c38ee9b17f22dfcb8240383cbf
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-12-25 11:59:12 +0530

    Pushed Animation Video

--- development/src/parts/hero.svelte
+++ development/src/parts/hero.svelte
@@ -1,3 +1,2 @@
 <section id="hero">
-    <h1 class="title">Shivendra Saurav</h1>
 </section>
--- development/src/parts/loader.svelte
+++ development/src/parts/loader.svelte
@@ -1,44 +1,8 @@
 <section id="loader">
-<pre>
-
-<div class="same_space">
-
-__________________________________________________________________________________________
-|                                                                                                                                                                    |
-|     ***********             ***                ***          ***          **               **      ************         ***            **********     <span style="margin-left: 1.5px;">|</span>
-|     **                       **  **             **  **      **  **         **               **      **          **        **  **              **           <span style="margin-left: 1px;">|</span>
-|     **                      **     **           **    **  **    **         **               **      **          **       **    **             **           <span style="margin-left: 1px;">|</span>
-|     ***********         **********          **     ****     **         **               **      ************     *********            **           <span style="margin-left: 2px;">|</span>
-|                 **        ***********         **                **          **            **       **        **       **********           **           <span style="margin-left: 4px;">|</span>
-|                 **       **           **        **                **            **        **         **         **     **          **          **           <span style="margin-left: 2px;">|</span>
-|     ***********       **            **        **                **             ********          **           **   **           **     *********      <span style="margin-left: 4px;">|</span>       
-|_________________________________________________________________________________________|
-
-</div>
-
-Booting System...
-
-Boot Sucessfull!!
-
-Loading Assests
-
-Image 1 - Hero
-Image 2 - Hero_Mobile
-
-Font 1 - Apex MK2
-Font 2 - Cyberpunk
-Font 3 - Vox Round
-Font 4 - Munro
-
-Stylesheet 1 - Main
-
-Svelte 1 - Main
-Svelte 2 - App
-Svelte 3 - Hero
-
-Assets Loaded Successfully
-
-Initializing Now...
-
-</pre>
+    <!-- svelte-ignore a11y-media-has-caption -->
+	<video
+		poster="https://sveltejs.github.io/assets/caminandes-llamigos.jpg"
+		src="https://sveltejs.github.io/assets/caminandes-llamigos.mp4" format="mp4" autoplay>
+			<track kind="captions"/>
+    </video>
 </section>
