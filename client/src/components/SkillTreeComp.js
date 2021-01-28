e1e6f354508b3871eb95a8f021e4d34dd16c6743
Shivendra Saurav <shivendrasaurav@gmail.com>
2021-01-29 01:33:08 +0530

    Made half of skill tree
    Todo:
    Complete Skill Tree
    Add Icons in Skill Tree

--- client/src/components/SkillTreeComp.js
+++ client/src/components/SkillTreeComp.js
@@ -1,6 +1,8 @@
 import React, { Fragment, useEffect } from "react";
 import "./styles/skills.css";
 
+import hw from "./static/skills/hw.png";
+
 function default_text(){
     document.getElementById("command").innerHTML = "SAMURAI &gt; ";
 }
@@ -14,74 +16,117 @@ const Skills = () =>{
         <Fragment>
             <section class="drawer">
                 <h2 style={{fontSize: "6vh", fontFamily: "Cyberpunk", color: "#1ecece", textShadow: "1px 4px #1e1e1e"}}>Skill Tree</h2><br/>
-                <ul className="root">
-                    <li>Hello World
-                        <ul className="nest">
-                            <li className="nest">Programming
-                            <ul className="cp">
-                                <li>C/C++</li>
-                                <li>Python</li>
-                            </ul>
-                            </li>
-                            <li className="nest">Full Stack Development
-                                <ul className="fed">
-                                    <li>Front End Development
-                                        <ul>
-                                            <li>HTML</li>
-                                            <li>CSS
-                                                <ul className="nest">
-                                                    <li>Bootstrap</li>
-                                                    <li>Fluent Design for Web</li>
-                                                    <li>Foundation</li>
-                                                    <li>Materialize</li>
-                                                    <li>Sass</li>
-                                                </ul>    
-                                            </li>
-                                            <li>JS
-                                                <ul className="nest">
-                                                    <li>JSON</li>
-                                                    <li>React
-                                                        <ul className="node">
-                                                            <li>Redux</li>
-                                                        </ul>
-                                                    </li>
-                                                    <li>Service Workers
-                                                        <ul className="node">
-                                                            <li>PWA</li>
-                                                            <li>Push Notifications</li>
-                                                        </ul>
-                                                    </li>
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                                <ul className="bed">
-                                    <li>Back End Development
-                                        <ul>
-                                            <li>DBMS
-                                                <ul className="nest">
-                                                    <li>RDBMS</li>
-                                                    <li>NoSQL</li>
-                                                </ul>    
-                                            </li>
-                                            <li>JS
-                                                <ul className="nest">
-                                                    <li>Node
-                                                        <ul className="node">
-                                                                <li>NPM</li>
-                                                                <li>Express</li>
-                                                        </ul>
-                                                    </li>
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
+                <table>
+                    <tr>
+                        <td>.</td>
+                        <td>.</td>
+                        <td>.</td>
+                        <td>.</td>
+                        <td>.</td>
+                        <td>.</td>
+                        <td>.</td>
+                        <td>.</td>
+                        <td>.</td>
+                        <td>.</td>
+                        <td>.</td>
+                        <td>.</td>
+                        <td>.</td>
+                        <td>.</td>
+                    </tr>
+                    <tr></tr>
+                    <tr></tr>
+                    <tr></tr>
+                    <tr></tr>
+                    <tr></tr>
+                    <tr></tr>
+                    <tr></tr>
+                    <tr></tr>
+                    <tr></tr>
+                    <tr>
+                        <td>.</td>
+                        <td>
+                            <img src={hw} alt="rn"/>
+                            <span><strong>React Native</strong><br/>Works similar to react, uses views and other objects to write native code</span>
+                        </td>
+                        <td>.</td>
+                        <td>.</td>
+                        <td>.</td>
+                        <td>.</td>
+                        <td colspan="3">
+                            <img src={hw} alt="fsd"/>
+                            <span><strong>Full Stack Development</strong><br/>Gives ability to work on both frontend and backend of a project</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>.</td>
+                        <td>.</td>
+                        <td colspan="2">
+                            <img src={hw} alt="and"/>
+                            <span><strong>Android</strong><br/>Ability to develop native apps for Android</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>.</td>
+                        <td>
+                            <img src={hw} class="locked" alt="flutter"/>
+                            <span><strong>Flutter</strong><br/>Unlocked by completing make it fly</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>.</td>
+                        <td>.</td>
+                        <td>.</td>
+                        <td colspan="2">
+                            <img src={hw} alt="nd"/>
+                            <span><strong>Native Development</strong><br/>Ability to develop native apps for mobile devices</span>
+                        </td>
+                        <td>.</td>
+                        <td colspan="3">
+                            <img src={hw} alt="hw"/>
+                            <span><strong>Hello World</strong><br/>This is where it all begins</span>
+                        </td>
+                        <td>.</td>
+                        <td colspan="2">
+                            <img src={hw} class="locked" alt="ml"/>
+                            <span><strong>Machine Learning</strong><br/>Unlocked by completing how machines learn</span>
+                        </td>
+                        <td>.</td>
+                        <td>.</td>
+                        <td>.</td>
+                    </tr>
+                    <tr></tr>
+                    <tr>
+                        <td>.</td>
+                        <td>.</td>
+                        <td colspan="2">
+                            <img src={hw} className="locked" alt="ios"/>
+                            <span><strong>iOS</strong><br/>Unlocked after buying a mac or macbook</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>.</td>
+                        <td>.</td>
+                        <td>.</td>
+                        <td>.</td>
+                        <td>.</td>
+                        <td>.</td>
+                        <td colspan="3">
+                            <img src={hw} alt="uiux"/>
+                            <span><strong>UI/UX Design</strong><br/>Gives ability to design User Interface or User Experience for a product</span>
+                        </td>
+
+                    </tr>
+                    <tr></tr>
+                    <tr></tr>
+                    <tr></tr>
+                    <tr></tr>
+                    <tr></tr>
+                    <tr></tr>
+                    <tr></tr>
+                    <tr></tr>
+                    <tr></tr>
+                    <tr></tr>
+                </table>
             </section>
         </Fragment>
     );
